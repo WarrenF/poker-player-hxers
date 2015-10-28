341050a6a5446074f236782b6b224d7a21f4148a
Richard Stevens <richard.stevens@holidayextras.com>
2015-10-28 11:52:06 +0000

    Updating branch for +1

--- player.js
+++ player.js
@@ -4,7 +4,9 @@ module.exports = {
   VERSION: "Awesome JS player",
 
   bet_request: function(game_state, bet) {
-    bet(1000);
+    var me = game_state.players[game_state.in_action];
+    var betAmount = game_state.current_buy_in - me.bet + 1;
+    bet(betAmount);
   },
 
   showdown: function(game_state) {
