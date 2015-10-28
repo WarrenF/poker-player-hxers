8190787c84fa91b0e71df8264217ca3e3899e576
Richard Stevens <richard.stevens@holidayextras.com>
2015-10-28 12:32:04 +0000

    Meet min

--- player.js
+++ player.js
@@ -10,12 +10,13 @@ module.exports = {
   VERSION: "Awesome JS player",
 
   bet_request: function(game_state, bet) {
-    var me = game_state.players[game_state.in_action];
-    var betAmount = game_state.current_buy_in - me.bet;
-    if( betAmount > me.stack ) {
-      bet( me.stack )
+    var play = Math.floor( Math.random( ) * 10 );
+    if( play > 3 ) {
+      bet( 0 );
       return;
     }
+    var me = game_state.players[game_state.in_action];
+    var betAmount = game_state.current_buy_in - me.bet + game_state.minimum_raise;
     bet(betAmount);
   },
 
